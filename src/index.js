a54c70043aa7643499481827d62141a19e956626
dirtdk <95955656+dirtdk@users.noreply.github.com>
2025-08-03 23:40:27 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; 
 import connectDB from "./configs/db.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -7,6 +8,14 @@ import { createPropertyIndex } from "./elasticSearch/esIndex.js";
 
 dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Create ES index if not exists
